2a7860d02a8ab3a842457d1d925da1038076cf3f
Will233 <s2573864@163.com>
2020-05-18 23:45:30 +0800

    feat(postcss): 添加function 配置支持

--- src/config/postcss.config.ts
+++ src/config/postcss.config.ts
@@ -2,9 +2,14 @@ import { getPostcssConfig } from '../common';
 
 type PostcssConfig = object & {
   plugins?: object;
-};
-
-function mergePostcssConfig(config1: PostcssConfig, config2: PostcssConfig) {
+}
+interface PostcssConfigFunction {
+  (ctx:{ file:object, options:object, env:string }): PostcssConfig
+}
+function mergePostcssConfig(config1: PostcssConfig, config2: PostcssConfig | PostcssConfigFunction) {
+  if (typeof config2 === 'function') {
+    return config2
+  }
   const plugins = {
     ...config1.plugins,
     ...config2.plugins,
